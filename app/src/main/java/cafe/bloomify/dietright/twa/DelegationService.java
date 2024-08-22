fb6a16ad1f365a938ef53765faa016ec22e9895e
fsnaix <truonggiangsn@gmail.com>
2024-08-23 02:33:13 +0800

    android build stuff do not pull

---
+++ app/src/main/java/cafe/bloomify/dietright/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package cafe.bloomify.dietright.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
